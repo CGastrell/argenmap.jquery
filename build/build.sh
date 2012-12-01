3d9467d74229ae076cad277cf47b19b33e1e3be3
Christian Gastrell <cgastrell@gmail.com>
2012-11-30 23:12:13 -0300

    agregue minify.sh que compilar argenmap.jquery con la api de google closure

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
-cat ../OpenLayers.js > argenmap.jquery.min.js
-cat ../argenmap.jquery.js >> argenmap.jquery.min.js
+./minify.sh
+cat ../OpenLayers-argenmap-closure.js > ../argenmap.jquery.min.js
+cat argenmap.jquery.min.sinopenlayers.js >> ../argenmap.jquery.min.js
+rm argenmap.jquery.min.sinopenlayers.js
 
