3c6fd864d83ecd96b465e456ccd3129c1e11e069
Christian Gastrell <cgastrell@gmail.com>
2012-12-01 05:24:49 -0300

    actualice al closure con simple optmizations porque adv quedaba perla

--- build/minify.sh
+++ build/minify.sh
@@ -5,7 +5,7 @@ in=../argenmap.jquery.js
 out=argenmap.jquery.min.sinopenlayers.js
  
 curl -s \
-  -d compilation_level=ADVANCED_OPTIMIZATIONS \
+  -d compilation_level=SIMPLE_OPTIMIZATIONS \
   -d output_format=text \
   -d output_info=compiled_code \
  --data-urlencode "js_code@${in}" \
