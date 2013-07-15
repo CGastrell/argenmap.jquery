110d7a7ff57da01cb37bcbf838d95b23b8ceb34e
Christian Gastrell <cg@ubuntu-vm.(none)>
2013-07-15 12:16:31 -0300

    init gh

--- build/build.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-./minify.sh
-./buildOpenLayers.sh
-cat ./OpenLayers-argenmap-closure.js > ../argenmap.jquery.min.js
-cat argenmap.jquery.min.sinopenlayers.js >> ../argenmap.jquery.min.js
-rm argenmap.jquery.min.sinopenlayers.js
-
