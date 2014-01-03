e526e0d86ee23d765f31fe88ed98a4ab7ee44058
Christian Gastrell <cg@ubuntu-vm.(none)>
2014-01-03 16:55:47 -0300

    modifica el script buildOpenLayers.sh

--- build/buildOpenLayers.sh
+++ build/buildOpenLayers.sh
@@ -1,6 +1,21 @@
 #/bin/bash
 
-cd ../lib/OpenLayers-2.12/build
-./build.py  -c closure ../../../build/argenmap.cfg ../../../build/OpenLayers-argenmap-closure.js
-cd ../../../build
+#ruta por defecto al directorio de OpenLayers
+WITH='../lib/OpenLayers-2.12'
 
+#ruta actual (para volver despues)
+BASE=$PWD
+
+#si se pasa un parametro se toma como ruta al directorio de OpenLayers
+if [ ! -z "$1" ] ; then
+	WITH=${1%/}
+fi
+
+#remover trailing slash y asignar a variable OLPATH
+OLPATH=$(echo $WITH | tr -d '\r')/build
+
+echo "Compilando desde $OLPATH"
+
+cd ${OLPATH}
+./build.py  -c closure ${BASE}/argenmap.cfg ${BASE}/OpenLayers-argenmap-closure.js
+cd ${BASE}
